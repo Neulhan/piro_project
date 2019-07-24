dfd6f3ad93ebb4d403ee29a0351a996f7a500361
neulhan <sting7675@gmail.com>
2019-07-24 16:22:53 +0900

    connect

--- django_team_project/quiz/models.py
+++ django_team_project/quiz/models.py
@@ -3,4 +3,31 @@ from django.db import models
 # Create your models here.
 
 
+class User(models.Model):
+    name = models.CharField(max_length=10)
+    code = models.IntegerField(max_length=6)
+    p1 = models.IntegerField(default=0)
+    p2 = models.IntegerField(default=0)
+    p3 = models.IntegerField(default=0)
+    p4 = models.IntegerField(default=0)
+    p5 = models.IntegerField(default=0)
+    p6 = models.IntegerField(default=0)
+    p7 = models.IntegerField(default=0)
+    p8 = models.IntegerField(default=0)
+    p9 = models.IntegerField(default=0)
+    p10 = models.IntegerField(default=0)
+
+
+class Guest(models.Model):
+    name = models.CharField(max_length=10)
+    b1 = models.BooleanField()
+    b2 = models.BooleanField()
+    b3 = models.BooleanField()
+    b4 = models.BooleanField()
+    b5 = models.BooleanField()
+    b6 = models.BooleanField()
+    b7 = models.BooleanField()
+    b8 = models.BooleanField()
+    b9 = models.BooleanField()
+    b10 = models.BooleanField()
 
--- django_team_project/quiz/views.py
+++ django_team_project/quiz/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .models import User, Guest
+from django.urls import reverse
 # Create your views here.
+
+
+def home(request):
+    return render(request, "layout.html")
+
+
+def next(request):
+    name = request.GET.get('name')
+    code = request.GET.get('code')
+    User.objects.create(name=name, code=code)
+
+    return render(request, reverse())
+
+
+
+
+

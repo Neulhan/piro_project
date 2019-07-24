07dfa10f470d1af9a46ae87f3dd952c84fa18793
neulhan <sting7675@gmail.com>
2019-07-24 20:59:22 +0900

    임시완성

--- django_team_project/django_team_project/urls.py
+++ django_team_project/django_team_project/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .. import quiz
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('quiz/', include('quiz.urls')),
-    path('quiz/<int:code>', quiz.views.guest_fisrt),
 
 ]

91838df8fe30cc72edef9c35321421cc122930ce
neulhan <sting7675@gmail.com>
2019-07-24 13:24:54 +0900

    models 에 default 값 추가

--- django_team_project/quiz/views.py
+++ django_team_project/quiz/views.py
@@ -1,1 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from .models import User, Guest
+
+
+
+
+
+
+
+def create_quiz1(request, pk):
+
+    return render(request,"create_quiz1.html", )
+
+
+def home(request):
+
+
+
+    User.objects.create(name= , code= ,)

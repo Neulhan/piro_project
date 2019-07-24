5fb318c933de1b64c50c754cabc51318aef8f0e3
neulhan <sting7675@gmail.com>
2019-07-24 12:08:20 +0900

    models 에 User & Guest를 추가하고 migrate

--- django_team_project/quiz/models.py
+++ django_team_project/quiz/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=10)
+    code = models.CharField(max_length=6)
+    p1 = models.CharField(max_length=10)
+    p2 = models.CharField(max_length=10)
+    p3 = models.CharField(max_length=10)
+    p4 = models.CharField(max_length=10)
+    p5 = models.CharField(max_length=10)
+    p6 = models.CharField(max_length=10)
+    p7 = models.CharField(max_length=10)
+    p8 = models.CharField(max_length=10)
+    p9 = models.CharField(max_length=10)
+    p10 = models.CharField(max_length=10)
+
+
+class Guest(models.Model):
+    name = models.CharField(max_length=10)
+    p1 = models.CharField(max_length=10)
+    p2 = models.CharField(max_length=10)
+    p3 = models.CharField(max_length=10)
+    p4 = models.CharField(max_length=10)
+    p5 = models.CharField(max_length=10)
+    p6 = models.CharField(max_length=10)
+    p7 = models.CharField(max_length=10)
+    p8 = models.CharField(max_length=10)
+    p9 = models.CharField(max_length=10)
+    p10 = models.CharField(max_length=10)

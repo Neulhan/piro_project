d586d03fe25dcdaf7f5c11775253334f0cfe98c7
neulhan <sting7675@gmail.com>
2019-07-24 14:34:00 +0900

    d

--- django_team_project/quiz/views.py
+++ django_team_project/quiz/views.py
@@ -3,7 +3,15 @@ from .models import User, Guest
 
 
 def home(request):
-    return render(request, "index.html")
+    print('test')
+    if request.method == "GET":
+        name = request.get('name')
+        code = request.get('code')
+        print(name, code)
+        next_url = 'http://127.0.0.1:8000/quiz/' + 'code' + '/create_quiz1'
+        return render(request, 'create_quiz1.html', {'name':name, 'code':code})
+    else:
+    return render(request, "index.html", {'next_url': next_url})
 
 
 def create_quiz1(request, quiz_value):
